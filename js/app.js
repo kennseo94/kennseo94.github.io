c503b692fc56722a48c92cb47581282467a140b3
Kenny Seo <32851778+seokenny@users.noreply.github.com>
2018-09-19 21:47:10 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,39 +1,91 @@
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
+var onezerothree = {
+	productName: "One Zero Three",
+	description: "Lily has never been quite like other people. Since she was a child, her wild imagination and strong memory have given her the talent to create her own home. A place in her mind away from the dangers of the world, to keep her dreams and memories safe. But during a night out with her friends, something happened. Something terrible and now her life has changed forever. Help Lily use her imagination to unlock her memories and relive the events of that night.",
+	image: "placeholder",
+	currentPledge: 9347,
+	currentBackers: 298,
+	timeLeft: function() {
+		var seconds = 1000,
+			minutes = seconds * 60,
+			hours = minutes * 60,
+			days = hours * 24;
+
+		var timer;
+
+		var end = new Date('09/26/2018 7:47AM'),
+			now = new Date();
+
+		var distance = end - now;
+
+		var days = Math.floor(distance / days),
+			hours = Math.floor((distance % days) / hours),
+			minutes = Math.floor((distance % hours) / minutes),
+			seconds = Math.floor((distance % minutes) / seconds);
+
+		return [days, hours, minutes, seconds];
+	},
+	pledgeLevels: [{
+		name: "I want the game!",
+		includes: [
+			"- Digital wallpaper pack",
+			"- Your name in the game credits",
+			"- Get the game via Steam"
+		],
+		available:  true
+	},
+	{
+		name: "I want the music too!",
+		includes: [
+			"- Digital wallpaper pack",
+			"- Your name in the game credits",
+			"- Get the game via Steam",
+			"- Official Soundtrack"
+		],
+		available: true
+	},
+	{
+		name: "I want it all!",
+		includes: [
+			"- Digital wallpaper pack",
+			"- Your name in the game credits",
+			"- The '103 Pack' (See description)"
+		],
+		available: true
+	}]
 }
 
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
+var countDownDate = new Date("Sept 26, 2018 7:47:25").getTime();
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var x = setInterval(function() {
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s to go!";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+}, 1000);
+
+document.getElementById("gameDesc").textContent = onezerothree.description;
+
+//PLEDGE 1
+document.getElementById("ksDesc1").textContent = onezerothree.pledgeLevels[0].name;
+
+//PLEDGE 2
+document.getElementById("ksDesc2").textContent = onezerothree.pledgeLevels[1].name;
+
+//PLEDGE 3
+document.getElementById("ksDesc3").textContent = onezerothree.pledgeLevels[2].name;
+
+document.getElementById("currPledged").textContent = onezerothree.currentPledge;
+
+document.getElementById("currentBackers").textContent = onezerothree.currentBackers;
