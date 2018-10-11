ee478b8a169be13e8defd5d4e0970cc2470308c9
Kenny Seo <32851778+seokenny@users.noreply.github.com>
2018-10-11 00:51:38 -0400

    Add files via upload
    Word Guessing

--- js/app.js
+++ js/app.js
@@ -1,91 +1,86 @@
-var onezerothree = {
-	productName: "One Zero Three",
-	description: "Lily has never been quite like other people. Since she was a child, her wild imagination and strong memory have given her the talent to create her own home. A place in her mind away from the dangers of the world, to keep her dreams and memories safe. But during a night out with her friends, something happened. Something terrible and now her life has changed forever. Help Lily use her imagination to unlock her memories and relive the events of that night.",
-	image: "placeholder",
-	currentPledge: 9347,
-	currentBackers: 298,
-	timeLeft: function() {
-		var seconds = 1000,
-			minutes = seconds * 60,
-			hours = minutes * 60,
-			days = hours * 24;
+var clickedLetter = "";
+var counter = 0
+var tries = 5;
 
-		var timer;
+var randomWords = ["ACRONYM", "BOUNCER", "CLOAKED", "DUNGEON", "EXCLUDE", "FAILURE", "GRAPPLE", "HEALTHY", "INGRAIN", "JOYLESS", "KITCHEN", "LIGHTEN", "MINUTES", "NUGGETS", "ORGANIC", "POLEAXE", "QUIETLY", "REBIRTH", "SCOWLED", "TIGHTLY", "UNLUCKY", "VECTORS", "WELFARE", "YOUNGER", "ZODIACS"];
 
-		var end = new Date('09/26/2018 7:47AM'),
-			now = new Date();
+var randomizer = Math.floor(Math.random() * Math.floor(randomWords.length))
 
-		var distance = end - now;
+var chosenWord = randomWords[randomizer].split("");
 
-		var days = Math.floor(distance / days),
-			hours = Math.floor((distance % days) / hours),
-			minutes = Math.floor((distance % hours) / minutes),
-			seconds = Math.floor((distance % minutes) / seconds);
-
-		return [days, hours, minutes, seconds];
-	},
-	pledgeLevels: [{
-		name: "I want the game!",
-		includes: [
-			"- Digital wallpaper pack",
-			"- Your name in the game credits",
-			"- Get the game via Steam"
-		],
-		available:  true
-	},
-	{
-		name: "I want the music too!",
-		includes: [
-			"- Digital wallpaper pack",
-			"- Your name in the game credits",
-			"- Get the game via Steam",
-			"- Official Soundtrack"
-		],
-		available: true
-	},
-	{
-		name: "I want it all!",
-		includes: [
-			"- Digital wallpaper pack",
-			"- Your name in the game credits",
-			"- The '103 Pack' (See description)"
-		],
-		available: true
-	}]
+function letterClicked(wasClicked){
+  clickedLetter = wasClicked;
+  console.log(clickedLetter);
+  var noLetter = 0;
+  var numRepeat = 0;
+  for(var i = 0; i < chosenWord.length; i++){
+    if(clickedLetter == chosenWord[i]){
+      document.getElementById("letter"+(i+1)).innerHTML = clickedLetter;
+      counter++;
+      numRepeat++;
+      addToCorrect(numRepeat);
+    }
+    else noLetter++;
+  }
+  if(noLetter == chosenWord.length){
+    tries--;
+    addToIncorrect();
+  }
+  removeLetter(wasClicked);
+  reduceBattery();
+  checkLives();
 }
 
-var countDownDate = new Date("Sept 26, 2018 7:47:25").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s to go!";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+function checkLives(){
+  if(tries == 0){
+    document.getElementById("winLoseStatus").innerHTML = "ACCESS DENIED";
+    document.getElementById("lockStatus").style.borderColor = "#cd5d5c";
+    document.getElementById("lockStatus").style.color = "#cd5d5c";
+    document.getElementById('lockedImage').classList.replace("show", "hidden");
+    document.getElementById('deniedImage').classList.replace("hidden", "show");
   }
-}, 1000);
+  if(counter == chosenWord.length){
+    document.getElementById("winLoseStatus").innerHTML = "ACCESS GRANTED";
+    document.getElementById("lockStatus").style.borderColor = "#6acc5d";
+    document.getElementById("lockStatus").style.color = "#6acc5d";
+    document.getElementById('lockedImage').classList.replace("show", "hidden");
+    document.getElementById('accessImage').classList.replace("hidden", "show");
+  }
+}
 
-document.getElementById("gameDesc").textContent = onezerothree.description;
+function reloadPage(){
+  window.location.reload();
+}
 
-//PLEDGE 1
-document.getElementById("ksDesc1").textContent = onezerothree.pledgeLevels[0].name;
+function reduceBattery(){
+  if(tries == 4){
+    document.querySelector(".batteryLife5").style.backgroundColor = "#717080";
+  }
+  else if(tries == 3){
+    document.querySelector(".batteryLife4").style.backgroundColor = "#717080";
+  }
+  else if (tries == 2){
+    document.querySelector(".batteryLife3").style.backgroundColor = "#717080";
+  }
+  else if (tries == 1){
+    document.querySelector(".batteryLife2").style.backgroundColor = "#717080";
+  }
+  else if (tries == 0){
+    document.querySelector(".batteryLife1").style.backgroundColor = "#717080";
+  }
+}
 
-//PLEDGE 2
-document.getElementById("ksDesc2").textContent = onezerothree.pledgeLevels[1].name;
+function addToCorrect(numRepeat){
+  if(numRepeat > 1){
+    return;
+  }
+  document.querySelector(".listOfCorrect").innerHTML += " <div class='usedLetterBox " + clickedLetter + "'>" + clickedLetter + "</div>";
+}
 
-//PLEDGE 3
-document.getElementById("ksDesc3").textContent = onezerothree.pledgeLevels[2].name;
+function addToIncorrect(){
+  document.querySelector(".listOfIncorrect").innerHTML += " <div class='usedLetterBox " + clickedLetter + "'>" + clickedLetter + "</div>";
+}
 
-document.getElementById("currPledged").textContent = onezerothree.currentPledge;
-
-document.getElementById("currentBackers").textContent = onezerothree.currentBackers;
+function removeLetter(wasClicked){
+  document.getElementById(wasClicked).style.backgroundColor = "#666";
+}
