a942fea5f90dde1047bc23cff888a32ee3d0243b
Kenny Seo <32851778+kennseo94@users.noreply.github.com>
2018-10-25 12:33:58 -0400

    Cookbook
    Cookbook branch

--- js/app.js
+++ js/app.js
@@ -1,86 +1,41 @@
-var clickedLetter = "";
-var counter = 0
-var tries = 5;
+$(document).ready(function(){
+	$("#spidercookiesentire").hide();
+	$("#pumpkinrollsentire").hide();
+	$("#mummypiesentire").hide();
 
-var randomWords = ["ACRONYM", "BOUNCER", "CLOAKED", "DUNGEON", "EXCLUDE", "FAILURE", "GRAPPLE", "HEALTHY", "INGRAIN", "JOYLESS", "KITCHEN", "LIGHTEN", "MINUTES", "NUGGETS", "ORGANIC", "POLEAXE", "QUIETLY", "REBIRTH", "SCOWLED", "TIGHTLY", "UNLUCKY", "VECTORS", "WELFARE", "YOUNGER", "ZODIACS"];
+	$(".brief_info").hide();
 
-var randomizer = Math.floor(Math.random() * Math.floor(randomWords.length))
-
-var chosenWord = randomWords[randomizer].split("");
-
-function letterClicked(wasClicked){
-  clickedLetter = wasClicked;
-  console.log(clickedLetter);
-  var noLetter = 0;
-  var numRepeat = 0;
-  for(var i = 0; i < chosenWord.length; i++){
-    if(clickedLetter == chosenWord[i]){
-      document.getElementById("letter"+(i+1)).innerHTML = clickedLetter;
-      counter++;
-      numRepeat++;
-      addToCorrect(numRepeat);
-    }
-    else noLetter++;
-  }
-  if(noLetter == chosenWord.length){
-    tries--;
-    addToIncorrect();
-  }
-  removeLetter(wasClicked);
-  reduceBattery();
-  checkLives();
-}
-
-function checkLives(){
-  if(tries == 0){
-    document.getElementById("winLoseStatus").innerHTML = "ACCESS DENIED";
-    document.getElementById("lockStatus").style.borderColor = "#cd5d5c";
-    document.getElementById("lockStatus").style.color = "#cd5d5c";
-    document.getElementById('lockedImage').classList.replace("show", "hidden");
-    document.getElementById('deniedImage').classList.replace("hidden", "show");
-  }
-  if(counter == chosenWord.length){
-    document.getElementById("winLoseStatus").innerHTML = "ACCESS GRANTED";
-    document.getElementById("lockStatus").style.borderColor = "#6acc5d";
-    document.getElementById("lockStatus").style.color = "#6acc5d";
-    document.getElementById('lockedImage').classList.replace("show", "hidden");
-    document.getElementById('accessImage').classList.replace("hidden", "show");
-  }
-}
-
-function reloadPage(){
-  window.location.reload();
-}
-
-function reduceBattery(){
-  if(tries == 4){
-    document.querySelector(".batteryLife5").style.backgroundColor = "#717080";
-  }
-  else if(tries == 3){
-    document.querySelector(".batteryLife4").style.backgroundColor = "#717080";
-  }
-  else if (tries == 2){
-    document.querySelector(".batteryLife3").style.backgroundColor = "#717080";
-  }
-  else if (tries == 1){
-    document.querySelector(".batteryLife2").style.backgroundColor = "#717080";
-  }
-  else if (tries == 0){
-    document.querySelector(".batteryLife1").style.backgroundColor = "#717080";
-  }
-}
-
-function addToCorrect(numRepeat){
-  if(numRepeat > 1){
-    return;
-  }
-  document.querySelector(".listOfCorrect").innerHTML += " <div class='usedLetterBox " + clickedLetter + "'>" + clickedLetter + "</div>";
-}
-
-function addToIncorrect(){
-  document.querySelector(".listOfIncorrect").innerHTML += " <div class='usedLetterBox " + clickedLetter + "'>" + clickedLetter + "</div>";
-}
-
-function removeLetter(wasClicked){
-  document.getElementById(wasClicked).style.backgroundColor = "#666";
-}
+	$(".first_recipe").click(function(){
+		$(".first_recipe").addClass("active");
+		$(".second_recipe").removeClass("active");
+		$(".third_recipe").removeClass("active");
+		$(".first_brief").slideDown(500);
+		$(".second_brief").hide();
+		$(".third_brief").hide();
+		$("#pumpkinrollsentire").hide();
+		$("#mummypiesentire").hide();
+		$("#spidercookiesentire").fadeIn(1150);
+	});
+	$(".second_recipe").click(function(){
+		$(".first_recipe").removeClass("active");
+		$(".second_recipe").addClass("active");
+		$(".third_recipe").removeClass("active");
+		$(".second_brief").slideDown(500);
+		$(".first_brief").hide();
+		$(".third_brief").hide();
+		$("#spidercookiesentire").hide();
+		$("#mummypiesentire").hide();
+		$("#pumpkinrollsentire").fadeIn(1150);
+	});
+	$(".third_recipe").click(function(){
+		$(".first_recipe").removeClass("active");
+		$(".second_recipe").removeClass("active");
+		$(".third_recipe").addClass("active");
+		$(".third_brief").slideDown(500);
+		$(".second_brief").hide();
+		$(".first_brief").hide();
+		$("#pumpkinrollsentire").hide();
+		$("#spidercookiesentire").hide();
+		$("#mummypiesentire").fadeIn(1150);
+	});
+});
