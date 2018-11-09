6ab63fc0517f5066a514c3d6aacc60b371f5a068
Kenny Seo <32851778+kennseo94@users.noreply.github.com>
2018-11-09 14:38:12 -0500

    Design tab
    Started the base of contact form and added a design tab for design work

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
 	$(".work_title").hide();
+	$(".design_tile").hide();
+	// $(".work1").on("mouseover", function(){
+	// 	$(".work1 .work_title").fadeIn();
+	// })
+	// $(".work1").on("mouseout", function(){
+	// 	$(".work1 .work_title").fadeOut();
+	// })
+	$(".work1").hover(function(){
+		$(".work1 .work_title").fadeToggle();
+		$(".work1 .work_descript").fadeToggle();
+	});
+	$(".work2").hover(function(){
+		$(".work2 .work_title").fadeToggle();
+		$(".work2 .work_descript").fadeToggle();
+	});
+	$(".work3").hover(function(){
+		$(".work3 .work_title").fadeToggle();
+		$(".work3 .work_descript").fadeToggle();
+	});
+	$(".work4").hover(function(){
+		$(".work4 .work_title").fadeToggle();
+		$(".work4 .work_descript").fadeToggle();
+	});
 
-	$(".work1").on("mouseenter", function(){
-		$(".work1 .work_title").fadeIn();
-		$(".work_descript").fadeIn();
-	})
-	$(".work1").on("mouseleave", function(){
-		$(".work1 .work_title").fadeOut();
-		$(".work_descript").fadeIn();
-	})
+	$(".design_tab").on('click', function(){
+		$(".work_tile").fadeOut(500, function(){
+			$(".design_tile").fadeIn(500)
+		});
+	});
 
-	$(".work2").on("mouseenter", function(){
-		$(".work2 .work_title").fadeIn();
-		$(".work_descript").fadeIn();
-	})
-	$(".work2").on("mouseleave", function(){
-		$(".work2 .work_title").fadeOut();
-		$(".work_descript").fadeIn();
-	})
-
-	$(".work3").on("mouseenter", function(){
-		$(".work3 .work_title").fadeIn();
-		$(".work_descript").fadeIn();
-	})
-	$(".work3").on("mouseleave", function(){
-		$(".work3 .work_title").fadeOut();
-		$(".work_descript").fadeIn();
-	})
-
-	$(".work4").on("mouseenter", function(){
-		$(".work4 .work_title").fadeIn();
-		$(".work_descript").fadeIn();
-	})
-	$(".work4").on("mouseleave", function(){
-		$(".work4 .work_title").fadeOut();
-		$(".work_descript").fadeIn();
+	$(".work_tab").on('click', function(){
+		$(".design_tile").fadeOut(500, function(){
+			$(".work_tile").fadeIn(500)
+		});
 	})
 });
